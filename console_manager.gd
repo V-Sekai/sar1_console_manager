6cccf26ab506fd24feecdfae07f0a28181d1852e
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-24 16:43:46 -0700

    Remove the console.

--- console_manager.gd
+++ console_manager.gd
@@ -13,7 +13,7 @@ func get_console() -> Node:
 	return console
 	
 func add_command(p_name: String, p_target: Node, p_target_name = null):
-	return console.add_command(p_name, p_target, p_target_name)
+	pass
 	
 func printl(p_string: String) -> void:
 	print(p_string)
@@ -24,21 +24,3 @@ func error(p_string: String) -> void:
 func fatal_error(p_string: String) -> void:
 	push_error("FATAL_ERROR: " + p_string)
 	
-func _console_toggled(p_console_toggled: bool) -> void:
-	if p_console_toggled:
-		console_is_active = true
-		console_active.emit(true)
-
-func _toggle_animation_finished(p_anim_name) -> void:
-	if !console.is_console_shown and p_anim_name == "fade":
-		console_active.emit(false)
-	
-func toggle_console() -> void:
-	console.toggle_console()
-		
-func _ready():
-	print("ConsoleManager _ready")
-	console = $"/root/Console"
-	console.toggled.connect(self._console_toggled)
-	print("Console is " + str(console) + " console._animationPlayer is " + str(console._animationPlayer))
-	console._animationPlayer.animation_finished.connect(self._toggle_animation_finished)
