a9a2c24bb69b286b840e033859a09c6ac0031e7e
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-24 17:34:29 -0700

    Remove console part of console manager.

--- console_manager_plugin.gd
+++ console_manager_plugin.gd
@@ -20,10 +20,8 @@ func _get_plugin_name() -> String:
 
 func _enter_tree() -> void:
 	editor_interface = get_editor_interface()
-	add_autoload_singleton("Console", "res://addons/sar1_console_manager/console.tscn")
 	add_autoload_singleton("ConsoleManager", "res://addons/sar1_console_manager/console_manager.gd")
 
 
 func _exit_tree() -> void:
 	remove_autoload_singleton("ConsoleManager")
-	remove_autoload_singleton("Console")
